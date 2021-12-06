8b005f67786c622c998fc2a3e8207af36d22591e
Leandro Martins <82072290+lecmartins@users.noreply.github.com>
2021-12-06 00:41:33 -0300

    construção do componente Square e implementação do onCLick alterando o estado do componente e mudando o texto renderizado

--- src/index.js
+++ src/index.js
@@ -2,24 +2,54 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Square extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      move: "",
+    };
+  }
+  
+  onClick() {
+    let newMove = this.state.move;
+
+    newMove === "X" ? newMove = "O" : newMove = "X";
+
+    this.setState({
+      move: newMove
+    });
+  }
+  
+  render() {
+    return (
+      <button className="square" onClick={() => this.onClick()}>
+        {this.state.move}
+      </button>
+    );
+  }
+}
+
 function Board(){
   return (
     <div>
       <div className="board-row">
-        <button className="square"></button>
-        <button className="square"></button>
-        <button className="square"></button>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className="board-row">
-        <button className="square"></button>
-        <button className="square"></button>
-        <button className="square"></button>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
       <div className="board-row">
-        <button className="square"></button>
-        <button className="square"></button>
-        <button className="square"></button>
+        <Square/>
+        <Square/>
+        <Square/>
       </div>
+
     </div>
   );
 }
